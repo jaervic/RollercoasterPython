5eb6abb35925b07bbd4daa141e17f79c8bffd6f9
Jaervic Guevara <jaervic@gmail.com>
2021-06-09 15:54:07 -0400

    Add files via upload

---
+++ Rollercoaster.py
@@ -0,0 +1,31 @@
+
+print("""
+
+Welcome to the Rollercoaster!!!
+
+""")
+
+height = int(input("What's your height in cm?: "))
+cost = 0
+
+if height >= 120:
+    print("Congrats you can ride the rollercoaster")
+    age = int(input("How old are you?: "))
+    if age < 12: 
+        cost += 5
+        print("Childs tickets cost $5")
+    elif age <= 18:
+        cost += 7
+        print("Youth tickets cost $7")
+    else:
+        cost += 12
+        print("Adults tickets costs $12")
+    pictures = input("Do you want pictures? it costs $3. Y or N: ")
+    if pictures == "Y" or pictures == "y":
+        cost += 3
+        print(f"Your total is {cost}$.")
+    else:
+        print(f"Your total is {cost}$.")
+        
+else:
+    print("You have to grow taller if you want to ride the rollercoaster")
